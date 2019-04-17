3b25f38e23f0a4fc8a05656cd6946f7ddefc623a
Kirill Kozlov <k.kozlov@itransition.com>
2019-04-17 15:30:12 +0300

    edit edit-image and loading-image component

--- memegenerator/ClientApp/src/app/edit-image/edit-image.component.ts
+++ memegenerator/ClientApp/src/app/edit-image/edit-image.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
+import "fabric";
 import fabric = require('fabric/fabric-impl');
 
 @Component({
@@ -9,22 +10,32 @@ import fabric = require('fabric/fabric-impl');
 })
 export class EditImageComponent {
 
-  private _canvas: fabric.Canvas;
-
-  @Input()
-  set canvas(canvas: fabric.Canvas) {
-    this._canvas = canvas;
-  }
-
-  get canvas(): fabric.Canvas { return this._canvas; }
-
+  canvas: fabric.Canvas;
   private text: fabric.Text;
-
+  private imageInstance: any;
+  uploadedImageUrl: string = '';
+  imageUploaded: boolean = false;
+  setMemePreview: boolean = false;
+  memePreview: string = '';
+  
   constructor() { }
 
-  addTextToCanvas() { this.addText(); }
+  addImageToCanvas() {
+    this.canvas = new fabric.Canvas('canvas');
 
-  private addText() {
+    var image = new Image();
+    image.onload = () => {
+      this.imageInstance = new fabric.Image(image);
+      this.canvas.setBackgroundImage(this.imageInstance, this.canvas.renderAll.bind(this.canvas), {
+        scaleY: this.canvas.getHeight() / this.imageInstance.height,
+        scaleX: this.canvas.getWidth() / this.imageInstance.width,
+        selectable: false
+      });
+    };
+    image.src = this.uploadedImageUrl;
+  }
+
+  onAddText() {
     var sampleText = 'Sample\ntext';
     this.text = new fabric.IText(sampleText, {
       fontFamily: 'Impact',
@@ -33,10 +44,33 @@ export class EditImageComponent {
       strokeWidth: 3,
       fill: '#ffffff',
       textAlign: 'center',
-      left: this._canvas.getWidth() / 2,
+      left: this.canvas.getWidth() / 2,
       originX: 'center'
     });
 
-    this._canvas.add(this.text);
+    this.canvas.add(this.text);
+  }
+
+  onDeleteText() {
+    this.canvas.remove(this.canvas.getActiveObject());
+  }
+
+  onClearCanvas() {
+    this.canvas.clear();
+    this.imageUploaded = false;
+  }
+
+  hideImageLoading() {
+    this.imageUploaded = true;
+  }
+
+  onGenerateMeme() {
+    this.setMemePreview = true;
+    this.memePreview = this.canvas.toDataURL();
+  }
+
+  onCreateNewMeme() {
+    this.setMemePreview = false;
+    this.imageUploaded = false;
   }
 }
--- memegenerator/ClientApp/src/app/loading-image/loading-image.component.ts
+++ memegenerator/ClientApp/src/app/loading-image/loading-image.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { EditImageComponent } from '../edit-image/edit-image.component';
 
-import "fabric"
 import fabric = require('fabric/fabric-impl');
 
 @Component({
@@ -11,14 +10,9 @@ import fabric = require('fabric/fabric-impl');
 })
 export class LoadingImageComponent {
 
-  canvas: fabric.Canvas;
-  private text: fabric.Text;
-  private imageInstance: any;
-  uploadedImageUrl: string = '';
-  condition: boolean = false;
-  setMemePreview: boolean = false;
-  memePreview: string = '';
-  
+  @ViewChild(EditImageComponent)
+  private editImage: EditImageComponent;
+
   constructor() { }
 
   onImageIsLoaded(event: any) {
@@ -26,65 +20,11 @@ export class LoadingImageComponent {
       var reader = new FileReader();
 
       reader.onload = (event: any) => {
-        this.uploadedImageUrl = event.target.result;
-        this.onAddImageToCanvas();
-        this.toggle();
+        this.editImage.uploadedImageUrl = event.target.result;
+        this.editImage.addImageToCanvas();
+        this.editImage.hideImageLoading();
       }
       reader.readAsDataURL(event.target.files[0]);
     }
-  }
-
-  onAddImageToCanvas() {
-    this.canvas = new fabric.Canvas('canvas');
-
-    var image = new Image();
-    image.onload = () => {
-      this.imageInstance = new fabric.Image(image);
-      this.canvas.setBackgroundImage(this.imageInstance, this.canvas.renderAll.bind(this.canvas), {
-          scaleY: this.canvas.getHeight() / this.imageInstance.height,
-          scaleX: this.canvas.getWidth() / this.imageInstance.width,
-          selectable: false
-      });
-    };
-    image.src = this.uploadedImageUrl;
-  }
-  
-  //onAddText() {
-  //  var sampleText = 'Sample\ntext';
-  //  this.text = new fabric.IText(sampleText, {
-  //    fontFamily: 'Impact',
-  //    fontSize: 50,
-  //    stroke: '#000000',
-  //    strokeWidth: 3,
-  //    fill: '#ffffff',
-  //    textAlign: 'center',
-  //    left: this.canvas.getWidth() / 2,
-  //    originX: 'center'
-  //  });
-
-  //  this.canvas.add(this.text);
-  //}
-
-  onDeleteText() {
-    this.canvas.remove(this.canvas.getActiveObject());
-  }
-
-  onClearCanvas() {
-    this.canvas.clear();
-    this.condition = false;
-  }
-
-  toggle() {
-    this.condition = true;
-  }
-
-  onGenerateMeme() {
-    this.setMemePreview = true;
-    this.memePreview = this.canvas.toDataURL();
-  }
-
-  onCreateNewMeme() {
-    this.setMemePreview = false;
-    this.condition = false;
   }
 }
