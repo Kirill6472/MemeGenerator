6ff507778dcc355c01684bf7e00aaa91d9a9a8dc
Kirill Kozlov <k.kozlov@itransition.com>
2019-06-21 16:04:41 +0300

    add a test for the Initialize method of the DbInitializer class

--- MemeGenerator.Tests/DbInitializerTests.cs
+++ MemeGenerator.Tests/DbInitializerTests.cs
@@ -1,3 +1,7 @@
+using Autofac.Extras.Moq;
+using MemeGenerator;
+using MemeGenerator.Services;
+using MemeGenerator.Services.InitialMemesProvider;
 using NUnit.Framework;
 
 namespace Tests
@@ -5,16 +9,20 @@ namespace Tests
     [TestFixture]
     public class DbInitializerTests
     {
-        [SetUp]
-        public void Setup()
-        {
-            
-        }
-
         [Test]
         public void Initialize_ObtainedDataAndDbContext_filledDb()
         {
-            
+            ImageTemplateList fakeimageTemplates = new ImageTemplateList();
+
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IInitialMemesProvider>().Setup(x => x.GetDataFromJson()).Returns(fakeimageTemplates);
+                DbInitializer dbInitializerStub = mock.Create<DbInitializer>();
+
+                dbInitializerStub.Initialize();
+
+                
+            }
         }
     }
 }
