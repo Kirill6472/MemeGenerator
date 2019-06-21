cdf6abe8e87231e8241564cea42d625163d5649f
Kirill Kozlov <k.kozlov@itransition.com>
2019-06-21 11:46:34 +0300

    Add MemeGenerator.Tests

---
+++ MemeGenerator.Tests/DbInitializerTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class DbInitializerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Initialize_ObtainedDataAndDbContext_filledDb()
+        {
+            
+        }
+    }
+}
