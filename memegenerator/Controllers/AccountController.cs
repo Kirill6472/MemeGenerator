16f911faf08a95c6df2eb81f912b09c8433e03db
Kirill Kozlov <k.kozlov@itransition.com>
2019-08-23 16:35:43 +0300

    add AccountController

---
+++ memegenerator/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemeGenerator.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public async Task LogIn(IdentityUser user)
+        {
+            await _signInManager.SignInAsync(user, isPersistent: false);
+        }
+
+        public async Task LogOut()
+        {
+            await _signInManager.SignOutAsync();
+        }
+    }
+}
